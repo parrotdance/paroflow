cfc71837c52eb7510906751f38ca1d4137894e33
ParroT <parrotdance95@gmail.com>
2020-06-12 16:07:33 +0800

    1. add multilines text support. 2. now can provide some options for every node and edge.

--- src/components/D3Chart.vue
+++ src/components/D3Chart.vue
@@ -13,18 +13,27 @@ export default class D3Chart extends Vue {
   }
 
   private renderChart(): void {
-    const flowChart = new FlowChart('#d3chart')
+    const flowChart = new FlowChart('#d3chart', {
+      lineColor: 'blue'
+    })
     flowChart
       .addNode('a', 50, 130, '测试')
-      .addNode('b', 150, 130, '测试2ab')
-      .addEdge('a', 'b', 'right-top')
-      .addNode('c', 350, 50, '测试测试测试测试试')
-      .addNode('d', 350, 270, '测试测试测试测试测试测试')
+      .addNode('b', 150, 130, '测试2ab', { backgroundColor: 'grey' })
+      .addEdge('a', 'b', { direction: 'right-top', color: 'red' })
+      .addNode('c', 350, 50, ['测试测试测试测试试', '测试测试测试测试试'], {
+        borderColor: 'yellow'
+      })
+      .addNode(
+        'd',
+        350,
+        270,
+        '测试测试测试\n测试测试测试测试\n测试测试测试测试\n测试测试测试测试'
+      )
       .addEdge('b', 'c')
       .addEdge('b', 'd')
       .addNode('e', 550, 130, '测试测试试')
-      .addEdge('c', 'e', 'right-top')
-      .addEdge('d', 'e', 'right-bottom')
+      .addEdge('c', 'e', { direction: 'right-top' })
+      .addEdge('d', 'e', { direction: 'right-bottom' })
     flowChart.render()
   }
 }
--- src/components/FlowChart.ts
+++ src/components/FlowChart.ts
@@ -1,41 +1,58 @@
 import { select } from 'd3-selection'
-import { path, Path } from 'd3-path'
+import { path } from 'd3-path'
 
-export interface Node {
+interface FlowChartInitialOptions {
+  width?: number
+  height?: number
+  lineColor?: string
+  fontSize?: number
+  lineHeight?: number
+}
+interface FlowChartNodeOptions {
+  padding?: [number, number]
+  textSize?: number
+  borderColor?: string
+  backgroundColor?: string
+}
+interface FlowChartEdgeOptions {
+  direction?: string
+  color?: string
+}
+interface Node {
   name: string
   center: [number, number]
   width: number
   height: number
   text: string[]
-  style: { [index: string]: any }
+  style: Dictionary
+  links: Link[]
 }
-export type NodeName = string
-export type EdgeDirection = 'top' | 'right' | 'bottom' | 'left'
-export interface EdgeOption {
-  name: NodeName
+type EdgeDirection = 'top' | 'right' | 'bottom' | 'left'
+interface EdgeOption {
+  name: string
   direction: EdgeDirection
 }
-export interface Edge {
+interface Edge {
   source: EdgeOption
   target: EdgeOption
-}
-interface FlowChartConfig {
-  nodes: Node[]
-  edges: Edge[]
+  style: Dictionary
 }
 interface Dictionary<T = any> {
   [index: string]: T
 }
 
-interface LinkNode extends Dictionary {
+interface Link extends Dictionary {
   source: Node
   target: Node
   outDir: EdgeDirection
   inDir: EdgeDirection
+  style: Dictionary
 }
 type Coordinate = [number, number]
+type GroupedLinks = { [index: string]: Link[] }
 
-const map: any = {
+// Define od-id-direction pattern
+const LINE_RENDER_PATTERN_MAP: any = {
   '1000100001000000': 'yx', // top-top-rt
   '1000100000010000': 'xy', // top-top-rb
   '1000100000000100': 'xy', // top-top-lb
@@ -111,165 +128,161 @@ const map: any = {
  *   [[ ],[ ],[ ]]  <- top/t
  *   [[ ],[ ],[ ]]
  *   [[ ],[ ],[ ]]  <- bottom/b
- *   by - ay >= 0 ? b : t
- *   bx - ax >= 0 ? r : l
+ *   by - ay >= 0 ? bottom : top
+ *   bx - ax >= 0 ? right : left
  */
 const DIR_MATRIX = [
   ['lt', 't', 'rt'],
   ['l', '', 'r'],
   ['lb', 'b', 'rb']
 ]
-
+const getDirection = (a: Coordinate, b: Coordinate) => {
+  const [ax, ay] = a
+  const [bx, by] = b
+  const ver = bx - ax >= 0 ? 2 : 0
+  const hor = by - ay >= 0 ? 2 : 0
+  return DIR_MATRIX[hor][ver]
+}
+const getBoundPoint = (
+  node: Node,
+  dir: EdgeDirection,
+  extend: boolean,
+  length = 12
+) => {
+  const {
+    center: [x, y],
+    width,
+    height
+  } = node
+  let dx = 0,
+    dy = 0
+  const extendLength = extend ? length : 0
+  if (dir === 'top') {
+    dy = -0.5 * height - extendLength
+  } else if (dir === 'right') {
+    dx = 0.5 * width + extendLength
+  } else if (dir === 'bottom') {
+    dy = 0.5 * height + extendLength
+  } else {
+    dx = -0.5 * width - extendLength
+  }
+  return [x + dx, y + dy] as Coordinate
+}
+const getTextLength = (text: string) => {
+  let len = 0
+  let i = 0
+  while (i < text.length) {
+    if (text.charCodeAt(i) > 127 || text.charCodeAt(i) == 94) {
+      len += 2
+    } else {
+      len++
+    }
+    i++
+  }
+  return len
+}
+/**
+ * Return a string of binary of u16 value.
+ * to represent relationship of outdir-indir-relative_dir
+ *
+ * example:
+ *   input: (right, left, rt)
+ *   output:
+ *  "0 1 0 0   0 0 0 1   0 1  0 0  0 0  0 0"
+ * -------------------------------------------
+ * | t r b l | t r b l | t rt r rb b lb l lt |
+ * | - - - - | - - - - | - -  - -  - -  - -  |
+ * |  outdir |  indir  |    relative_dir     |
+ * -------------------------------------------
+ */
+const getArrowTypeSign = (
+  outDir: EdgeDirection,
+  inDir: EdgeDirection,
+  dir: string
+) => {
+  let sign = 0
+  switch (outDir) {
+    case 'top':
+      sign += 1 << 15
+      break
+    case 'right':
+      sign += 1 << 14
+      break
+    case 'bottom':
+      sign += 1 << 13
+      break
+    case 'left':
+      sign += 1 << 12
+      break
+  }
+  switch (inDir) {
+    case 'top':
+      sign += 1 << 11
+      break
+    case 'right':
+      sign += 1 << 10
+      break
+    case 'bottom':
+      sign += 1 << 9
+      break
+    case 'left':
+      sign += 1 << 8
+      break
+  }
+  switch (dir) {
+    case 'rt':
+      sign += 1 << 6
+      break
+    case 'rb':
+      sign += 1 << 4
+      break
+    case 'lb':
+      sign += 1 << 2
+      break
+    case 'lt':
+      sign += 1 << 0
+      break
+  }
+  return sign.toString(2).padStart(16, '0')
+}
 class FlowChart {
   private _svg?: any
   private nodes: Node[] = []
   private edges: Edge[] = []
-  private LINE_COLOR = '#47b785'
-  constructor(el: string, width = 800, height = 600) {
-    const attr = { width, height }
-    this._svg = select(el)
-    Object.entries(attr).forEach(([k, v]) => this._svg.attr(k, v))
+  private options: any = {
+    width: 800,
+    height: 600,
+    lineColor: '#47b785',
+    fontSize: 14,
+    lineHeight: 24
   }
-  private getBoundPoint(
-    node: Node,
-    dir: EdgeDirection,
-    extend: boolean,
-    length = 12
-  ) {
-    const {
-      center: [x, y],
-      width,
-      height
-    } = node
-    let dx = 0,
-      dy = 0
-    const extendLength = extend ? length : 0
-    if (dir === 'top') {
-      dy = -0.5 * height - extendLength
-    } else if (dir === 'right') {
-      dx = 0.5 * width + extendLength
-    } else if (dir === 'bottom') {
-      dy = 0.5 * height + extendLength
-    } else {
-      dx = -0.5 * width - extendLength
-    }
-    return [x + dx, y + dy] as Coordinate
-  }
-  private getDirection(a: Coordinate, b: Coordinate) {
-    const [ax, ay] = a
-    const [bx, by] = b
-    const ver = bx - ax >= 0 ? 2 : 0
-    const hor = by - ay >= 0 ? 2 : 0
-    return DIR_MATRIX[hor][ver]
-  }
-  private getTextLength(text: string): number {
-    let len = 0,
-      i = 0
 
-    while (i < text.length) {
-      if (text.charCodeAt(i) > 127 || text.charCodeAt(i) == 94) {
-        len += 2
-      } else {
-        len++
+  constructor(el: string, options: FlowChartInitialOptions = {}) {
+    Object.entries(options).forEach(([k, v]) => {
+      if (this.options[k]) {
+        this.options[k] = v
       }
-      i++
-    }
-    return len
+    })
+    const { width, height } = this.options
+    this._svg = select(el)
+    this._svg.attr('width', width)
+    this._svg.attr('height', height)
   }
-  private getArrowTypeSign(
-    outDir: EdgeDirection,
-    inDir: EdgeDirection,
-    dir: string
-  ) {
-    let sign = 0
-    switch (outDir) {
-      case 'top':
-        sign += 1 << 15
-        break
-      case 'right':
-        sign += 1 << 14
-        break
-      case 'bottom':
-        sign += 1 << 13
-        break
-      case 'left':
-        sign += 1 << 12
-        break
-    }
-    switch (inDir) {
-      case 'top':
-        sign += 1 << 11
-        break
-      case 'right':
-        sign += 1 << 10
-        break
-      case 'bottom':
-        sign += 1 << 9
-        break
-      case 'left':
-        sign += 1 << 8
-        break
-    }
-    switch (dir) {
-      case 'rt':
-        sign += 1 << 6
-        break
-      case 'rb':
-        sign += 1 << 4
-        break
-      case 'lb':
-        sign += 1 << 2
-        break
-      case 'lt':
-        sign += 1 << 0
-        break
-    }
-    return sign.toString(2).padStart(16, '0')
-  }
-  private drawArrow(p: Path, ep: Coordinate, inDir: EdgeDirection, size = 6) {
-    let [x1, y1] = ep,
-      [x2, y2] = ep
-    switch (inDir) {
-      case 'top':
-        x1 -= size
-        y1 -= size
-        x2 += size
-        y2 -= size
-        break
-      case 'right':
-        x1 += size
-        y1 -= size
-        x2 += size
-        y2 += size
-        break
-      case 'bottom':
-        x1 -= size
-        y1 += size
-        x2 += size
-        y2 += size
-        break
-      case 'left':
-        x1 -= size
-        y1 -= size
-        x2 -= size
-        y2 += size
-        break
-    }
-    p.moveTo(x1, y1)
-    p.lineTo(...ep)
-    p.lineTo(x2, y2)
-  }
-  private draw(p: Path, linkNode: any) {
-    const { source, target, outDir, inDir } = linkNode
+  private drawLinkLine(group: any, link: any) {
+    const p = path()
+    const pathEl = group.append('path')
+    const { style } = link
+    pathEl.attr('stroke', style.color).attr('fill', 'transparent')
+    const { source, target, outDir, inDir } = link
     // 1. get point coordinates at extend line
-    const startPoint = this.getBoundPoint(source, outDir, false)
-    const endPoint = this.getBoundPoint(target, inDir, false)
-    const startExtendPoint = this.getBoundPoint(source, outDir, true)
-    const endExtendPoint = this.getBoundPoint(target, inDir, true)
+    const startPoint = getBoundPoint(source, outDir, false)
+    const endPoint = getBoundPoint(target, inDir, false)
+    const startExtendPoint = getBoundPoint(source, outDir, true)
+    const endExtendPoint = getBoundPoint(target, inDir, true)
     // 2. compute target node direction: lt/rt/lb/rb. TODO: t/b/l/r
-    const arrowDir = this.getDirection(startExtendPoint, endExtendPoint)
-    // 3. define line path to match od-id-direction and run
-    const typeSign = this.getArrowTypeSign(outDir, inDir, arrowDir)
+    const arrowDir = getDirection(startExtendPoint, endExtendPoint)
+    // 3. define line path to match od-id-direction pattern and run
+    const typeSign = getArrowTypeSign(outDir, inDir, arrowDir)
 
     const [x1, y1] = startPoint
     const [x2, y2] = endPoint
@@ -277,7 +290,7 @@ class FlowChart {
     const [tx, ty] = endExtendPoint
     p.moveTo(x1, y1)
     p.lineTo(sx, sy)
-    const drawPattern = map[typeSign]
+    const drawPattern = LINE_RENDER_PATTERN_MAP[typeSign]
     switch (drawPattern) {
       case 'xy':
         p.lineTo(tx, sy)
@@ -298,35 +311,79 @@ class FlowChart {
         p.lineTo(tx, ty)
     }
     p.lineTo(x2, y2)
-    this.drawArrow(p, endPoint, inDir)
+    // draw arrow
+    {
+      let [x1, y1] = endPoint
+      let [x2, y2] = endPoint
+      const se = 5 // short edge
+      const le = 7 // long edge
+      switch (inDir) {
+        case 'top':
+          x1 -= se
+          y1 -= le
+          x2 += se
+          y2 -= le
+          break
+        case 'right':
+          x1 += le
+          y1 -= se
+          x2 += le
+          y2 += se
+          break
+        case 'bottom':
+          x1 -= se
+          y1 += le
+          x2 += se
+          y2 += le
+          break
+        case 'left':
+          x1 -= le
+          y1 -= se
+          x2 -= le
+          y2 += se
+          break
+      }
+      p.moveTo(x1, y1)
+      p.lineTo(...endPoint)
+      p.lineTo(x2, y2)
+    }
+    pathEl.attr('d', p.toString())
   }
   public addNode(
     name: string,
     x: number,
     y: number,
     text: string | string[],
-    options: any = {}
+    options: FlowChartNodeOptions = {}
   ): this {
-    const { padding = [10, 20], textSize = 14, lineHeight = 18 } = options
+    const {
+      padding = [10, 20],
+      textSize = this.options.fontSize,
+      borderColor = this.options.lineColor,
+      backgroundColor = 'transparent'
+    } = options
     const MIN_HEIGHT = 50
     const MIN_WIDTH = 60
-    let lines = 0,
-      maxTextLength = 0,
-      textSpl
+    let lines = 0
+    let maxTextLength = 0
+    let textSpl = null
     if (typeof text === 'string') {
       textSpl = text.split('\n')
       lines = textSpl.length
-      maxTextLength = Math.max(...textSpl.map(str => this.getTextLength(str)))
+      maxTextLength = Math.max(...textSpl.map(str => getTextLength(str)))
     } else {
       textSpl = text
       lines = text.length
-      maxTextLength = Math.max(...text.map(str => this.getTextLength(str)))
+      maxTextLength = Math.max(...text.map(str => getTextLength(str)))
     }
     const width = Math.max(
       (textSize / 2) * maxTextLength + padding[1] * 2,
       MIN_WIDTH
     )
-    const height = Math.max(lines * lineHeight + padding[0] * 2, MIN_HEIGHT)
+    const height = Math.max(
+      lines * this.options.lineHeight + padding[0] * 2,
+      MIN_HEIGHT
+    )
     this.nodes.push({
       name,
       center: [x, y],
@@ -334,14 +391,30 @@ class FlowChart {
       height,
       text: textSpl,
       style: {
-        textSize: textSize + 'px',
-        lineHeight: lineHeight + 'px'
-      }
+        padding,
+        textSize,
+        borderColor,
+        backgroundColor
+      },
+      links: []
     })
     return this
   }
-  public addEdge(source: string, target: string, direction = 'right-left') {
-    const [outDir, inDir] = direction.split('-')
+  public addEdge(
+    source: string,
+    target: string,
+    options: FlowChartEdgeOptions = {}
+  ) {
+    const { direction, color = this.options.lineColor } = options
+    const defaultDir = 'right-left'
+    let outDir = ''
+    let inDir = ''
+    if (direction && !direction.includes('-')) {
+      console.error(
+        `invalid direction: ${direction}, use default direction instead.`
+      )
+    }
+    ;[outDir, inDir] = direction ? direction.split('-') : defaultDir.split('-')
     const from: EdgeOption = {
       name: source,
       direction: outDir as EdgeDirection
@@ -352,78 +425,97 @@ class FlowChart {
     }
     this.edges.push({
       source: from,
-      target: to
+      target: to,
+      style: {
+        color
+      }
     })
     return this
   }
   public render() {
-    const nodeMap: LinkNode = Array.from(this.nodes).reduce(
-      (map, node) =>
-        Object.assign(map, { [node.name]: { ...node, linkNodes: [] } }),
-      {} as LinkNode
+    const nodeMap: Dictionary<Node> = this.nodes.reduce(
+      (map, node) => Object.assign(map, { [node.name]: node }),
+      {} as Dictionary<Node>
     )
     this.edges.forEach(edge => {
-      const { source, target } = edge
+      const { source, target, style } = edge
       const sourceNode = nodeMap[source.name]
       const targetNode = nodeMap[target.name]
       if (sourceNode === undefined) {
-        throw new Error(`Can not find Edge source node: ${sourceNode.name}.`)
+        throw new Error(
+          `Can not find Edge source node: ${(sourceNode as Node).name}.`
+        )
       }
       if (targetNode === undefined) {
-        throw new Error(`Can not find Edge source node: ${targetNode.name}.`)
+        throw new Error(
+          `Can not find Edge source node: ${(targetNode as Node).name}.`
+        )
       }
-      sourceNode.linkNodes.push({
+      sourceNode.links.push({
         target: targetNode,
         outDir: source.direction,
         inDir: target.direction,
-        source: sourceNode
+        source: sourceNode,
+        style
       })
     })
-    const g = this._svg
-      .selectAll('g')
-      .data(Object.values(nodeMap))
-      .enter()
-      .append('g')
-    g.append('rect')
-      .attr('class', 'node-rect')
-      .attr('x', (d: Node) => d.center[0] - d.width / 2)
-      .attr('y', (d: Node) => d.center[1] - d.height / 2)
-      .attr('width', (d: Node) => d.width)
-      .attr('height', (d: Node) => d.height)
-      .attr('stroke', this.LINE_COLOR)
-      .attr('fill', 'transparent')
-      .attr('stroke-width', '2')
-      .attr('rx', '4')
-      .attr('ry', '4')
-    g.append('text')
-      .attr('x', (d: Node) => d.center[0])
-      .attr('y', (d: Node) => d.center[1])
-      .attr('text-anchor', 'middle')
-      .style('font-size', '14px')
-      .text((d: Node) => d.text || '')
-    g.append('path')
-      .attr('stroke', this.LINE_COLOR)
-      .attr('fill', 'transparent')
-      .attr('d', (source: any) => {
-        const p = path()
-
-        type GroupedLinkNodes = { [index: string]: LinkNode[] }
-        const groupedLinkNodes: GroupedLinkNodes = source.linkNodes.reduce(
-          (res: GroupedLinkNodes, linkNode: LinkNode) => {
-            if (res[linkNode.outDir]) {
-              res[linkNode.outDir].push(linkNode)
-            } else {
-              res[linkNode.outDir] = [linkNode]
-            }
-            return res
-          },
-          {} as GroupedLinkNodes
-        )
-        Object.values(groupedLinkNodes).forEach((linkNodes: LinkNode[]) =>
-          linkNodes.forEach(n => this.draw(p, n))
-        )
-        return p.toString()
+    Object.values(nodeMap).forEach(d => {
+      const g = this._svg.append('g')
+      const { style } = d
+      // render rect
+      g.append('rect')
+        .attr('class', 'node-rect')
+        .attr('x', d.center[0] - d.width / 2)
+        .attr('y', d.center[1] - d.height / 2)
+        .attr('width', d.width)
+        .attr('height', d.height)
+        .attr('stroke', style.borderColor)
+        .attr('fill', style.backgroundColor)
+        .attr('stroke-width', '2')
+        .attr('rx', '4')
+        .attr('ry', '4')
+      // render text
+      const t = g
+        .append('text')
+        .attr('text-anchor', 'middle')
+        .attr('font-size', 14)
+      let textPosYOffset = 0
+      const textLines = d.text.length
+      if (textLines === 1) {
+        textPosYOffset = 0
+      } else if (textLines % 2 !== 0) {
+        textPosYOffset = ((1 - textLines) / 2) * this.options.lineHeight
+      } else {
+        textPosYOffset = (0.5 - textLines / 2) * this.options.lineHeight
+      }
+      const STATIC_OFFSET = 5 // I don't know why
+      d.text.forEach((str: string, i: number) => {
+        const y =
+          d.center[1] +
+          textPosYOffset +
+          i * this.options.lineHeight +
+          STATIC_OFFSET
+        t.append('tspan')
+          .attr('x', d.center[0])
+          .attr('y', y)
+          .text(str)
       })
+      // render link line
+      const groupedLinks: GroupedLinks = d.links.reduce(
+        (res: GroupedLinks, link: Link) => {
+          if (res[link.outDir]) {
+            res[link.outDir].push(link)
+          } else {
+            res[link.outDir] = [link]
+          }
+          return res
+        },
+        {} as GroupedLinks
+      )
+      Object.values(groupedLinks).forEach((links: Link[]) => {
+        links.forEach(n => this.drawLinkLine(g, n))
+      })
+    })
   }
 }
 
